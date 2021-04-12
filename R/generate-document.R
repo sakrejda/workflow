20f072738f2f9a2196ec91f64038c4315f87d708
Krzysztof Sakrejda <krzysztof.sakrejda@gmail.com>
2021-04-12 08:33:38 -0400

    If the template doesn't exist, abort.

--- R/generate-document.R
+++ R/generate-document.R
@@ -17,10 +17,12 @@ generate_document = function(template, rel_output_path, ..., type = 'html') {
   file_name = fs::path_file(template)
   out_path = workflow::artifact_file(rel_output_path, fs::path_ext_set(file_name, type))
   template_path = workflow::project_file(template)
+  if (!fs::file_exists(template_path)) {
+    rlang::abort(msg = glue::glue("template not found: '{file}'", file = template_path),
+                 missing_file = template_path)
+  }
   output_dir = workflow::artifact_dir(rel_output_path)
-  fs::dir_create(path = output_dir, recurse = TRUE)
   build_dir = workflow::build_dir(rel_output_path)
-  fs::dir_create(path = build_dir, recurse = TRUE)
   rmarkdown::render(
     input = template_path,
     output_dir = output_dir,
