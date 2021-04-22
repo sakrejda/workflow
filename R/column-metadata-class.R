b70aafa659817b387d2faab752f237a0a7fc276c
Krzysztof Sakrejda <krzysztof.sakrejda@gmail.com>
2021-04-22 12:34:50 -0400

    Expanded column metadata

--- R/column-metadata-class.R
+++ R/column-metadata-class.R
@@ -16,13 +16,17 @@ ColumnMetadata = R6::R6Class(classname = "ColumnMetadata",
       definition = NA, 
       standard_name = standardize_name(name),
       format = NA,
-      units = NA
+      units = NA,
+      values = NA,
+      original_name = NA
     ) {
       private$.name = name
       private$.definition = definition
       private$.standard_name = standard_name
+      private$.original_name = original_name
       private$.format = format
       private$.units = units
+      private$.values = values
     },
     merge = function(other, priority = 'self') {
       if (self$name == other$name && priority == 'self') {
@@ -38,6 +42,12 @@ ColumnMetadata = R6::R6Class(classname = "ColumnMetadata",
         if (is.na(self$units)) {
           self$units = other$units
         }
+        if (is.na(self$values)) {
+          self$values = other$values
+        }
+        if (is.na(self$original_name)) {
+          self$original_name = other$original_name
+        }
         return(self)
       } else if (self$name == other$name && priority == 'other') {
         if (!is.na(other$definition)) {
@@ -52,6 +62,12 @@ ColumnMetadata = R6::R6Class(classname = "ColumnMetadata",
         if (!is.na(other$units)) {
           self$units = other$units
         }
+        if (is.na(self$values)) {
+          self$values = other$values
+        }
+        if (!is.na(other$original_name)) {
+          self$original_name = other$original_name
+        }
         return(self)
       } else if (!is.na(self$standard_name) && (self$standard_name == other$name)) {
         self$standard_name = other$standard_name
@@ -64,6 +80,12 @@ ColumnMetadata = R6::R6Class(classname = "ColumnMetadata",
         if (!is.na(other$units)) {
           self$units = other$units
         }
+        if (!is.na(other$values)) {
+          self$values = other$values
+        }
+        if (!is.na(other$original_name)) {
+          self$original_name = other$original_name
+        }
         return(self)
       } else if (!is.na(other$standard_name) && (other$standard_name == self$name)) {
         self$name = other$name
@@ -75,6 +97,12 @@ ColumnMetadata = R6::R6Class(classname = "ColumnMetadata",
         }
         if (is.na(self$units)) {
           self$units = other$units
+        }
+        if (is.na(self$values)) {
+          self$values = other$values
+        }
+        if (is.na(self$original_name)) {
+          self$original_name = other$original_name
         }
         return(self)
       } else if (!is.na(self$standard_name) && !is.na(other$standard_name) &&
@@ -94,22 +122,29 @@ ColumnMetadata = R6::R6Class(classname = "ColumnMetadata",
       s = glue::glue("Name: {private$.name}\n", 
                      "Definition: {private$.definition}\n",
                      "Standard name: {private$.standard_name}\n",
+                     "Original name: {private$.original_name}\n",
                      "Format: {private$.format}\n",
-                     "Units: {private$.units}\n")
+                     "Units: {private$.units}\n",
+                     "Values: {private$.values}\n"
+      )
       return(s)
     },
     as_list = function() list(name = private$.name, 
                               definition = private$.definition, 
                               standard_name = private$.standard_name,
+                              original_name = private$.original_name,
                               format = private$.format,
-                              units = private$.units)
+                              units = private$.units,
+                              values = private$.values)
   ),
   private = list(
     .name = character(),
     .definition = character(),
     .standard_name = character(),
+    .original_name = character(),
     .format = character(),
-    .units = character()
+    .units = character(),
+    .values = character()
   ),
   active = list(
     name = function(x) {
@@ -133,6 +168,13 @@ ColumnMetadata = R6::R6Class(classname = "ColumnMetadata",
         private$.standard_name = x
       }
     },
+    original_name = function(x) {
+      if (missing(x)) {
+        return(private$.original_name)
+      } else {
+        private$.original_name = x
+      }
+    },
     format = function(x) {
       if (missing(x)) {
         return(private$.format)
@@ -145,6 +187,13 @@ ColumnMetadata = R6::R6Class(classname = "ColumnMetadata",
         return(private$.units)
       } else {
         private$.units = x
+      }
+    },
+    values = function(x) {
+      if (missing(x)) {
+        return(private$.values)
+      } else {
+        private$.values = x
       }
     }
   )
