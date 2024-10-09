fd652df2e70626339967631cfd72d2f5a5acfb08
Krzysztof Sakrejda <krzysztof.sakrejda@gmail.com>
2024-10-09 16:19:21 -0400

    flake changes

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.rPackages.buildRPackage {
       name = "workflow";
       src = fetchGit {
-        name = "workflow";
+        name = "workflow-source";
         url = "git+ssh://git@fawkes.io:2222/data/git/git/workflow";
         ref = "main";
         rev = "a789d8b818f8e8e22369b74fd77cbb0905b9ad02";
